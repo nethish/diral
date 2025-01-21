de1c3a7d9c1c438629255582d2fae00e0ee78b6f
Nethish <nethish259@gmail.com>
2025-01-22 02:10:42 +0530

    update cd command

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 mkdir -p ~/.local/bin/
 cp ./diral.py ~/.local/bin/diral
-# cp ./diral ~/.local/bin/diral
 
 if [[ ":$PATH:" != *"/.local/bin"* ]]; then
   echo 'export PATH=~/.local/bin:$PATH' >> ~/.bashrc
   echo 'export PATH=~/.local/bin:$PATH' >> ~/.zshrc
 fi
 
+cat ./cd >> ~/.bashrc
+cat ./cd >> ~/.zshrc
